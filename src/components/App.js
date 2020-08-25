0fa74659f718680aa68a949b7603b91761c6889a
Jose Jimenez <58991130+jjdevco@users.noreply.github.com>
2020-08-24 20:17:41 -0500

    add react-router-dom, scal components

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,27 @@
 import React from "react";
 
+// Router components
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 //Preset Theme
 import { ThemeProvider } from "emotion-theming";
 import theme from "@rebass/preset";
 
+import Home from "./Home";
+import Login from "./Login";
+import Register from "./Register";
+
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <div>
-        <header>
-          <h2>LLab</h2>
-        </header>
-      </div>
-    </ThemeProvider>
+    <Router>
+      <ThemeProvider theme={theme}>
+        <Switch>
+          <Route component={Home} path="/" exact />
+          <Route component={Login} path="/login" exact />
+          <Route component={Register} path="/register" exact />
+        </Switch>
+      </ThemeProvider>
+    </Router>
   );
 }
 
