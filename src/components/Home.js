8c2e9bcdb3eaeb7ce7d2fc26ec736aa340bd229b
Jose Jimenez <58991130+jjdevco@users.noreply.github.com>
2020-08-25 19:22:36 -0500

    add functionality for Home Component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,85 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import { getPosts } from "../api";
+
+import InfiniteScroll from "react-infinite-scroll-component";
+import { Eclipse, DualRing } from "react-loading-io";
+import { Flex } from "rebass";
+
+import Post from "./Post";
 
 export default (props) => {
-  return <h1>Home</h1>;
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [totalPages, setTotalPages] = useState(false);
+
+  const setAllData = (data, page, limit, total) => {
+    setPosts((posts) => posts.concat(data));
+    setCurrentPage(page);
+    setTotalPages(Math.ceil(total / limit));
+  };
+
+  const fetchMorePosts = async () => {
+    try {
+      const { data, page, limit, total } = await getPosts(currentPage + 1);
+      setAllData(data, page, limit, total);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    const fetcher = async () => {
+      try {
+        const { data, page, limit, total } = await getPosts(0);
+        setAllData(data, page, limit, total);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetcher();
+  }, []);
+
+  return (
+    <Flex
+      sx={{
+        maxWidth: ["100%", "600px"],
+        overflowY: "hidden",
+      }}
+      flex="1 1 auto"
+      flexDirection="column"
+      p={2}
+      mx="auto"
+      bg="transparent"
+    >
+      {loading ? (
+        <Flex flex="1 1 auto" justifyContent="center" alignItems="center">
+          <Eclipse color="#acb8b4" size={120} width={4} />
+        </Flex>
+      ) : (
+        <InfiniteScroll
+          style={{ overflow: "hidden" }}
+          dataLength={posts.length}
+          next={fetchMorePosts}
+          hasMore={currentPage < totalPages}
+          endMessage={
+            <p style={{ textAlign: "center" }}>
+              <b>Yay! You have seen it all</b>
+            </p>
+          }
+          loader={
+            <Flex justifyContent="center" alignItems="center">
+              <DualRing color="#acb8b4" size={64} width={2} />
+            </Flex>
+          }
+        >
+          {posts.map((post, index) => (
+            <Post key={post.id} data={post} />
+          ))}
+        </InfiniteScroll>
+      )}
+    </Flex>
+  );
 };
